5b3b345e3752958dbc1a3853703089933787a64d
bforddev7 <bforddev7@gmail.com>
2023-10-13 18:01:23 -0500

    feat: continued work on community page for creating posts

--- src/app/(crumbbasket)/c/[crumbbasket]/page.tsx
+++ src/app/(crumbbasket)/c/[crumbbasket]/page.tsx
@@ -1,12 +1,80 @@
 // pull dynamic routes from crumbit.com/c/[...crumbbasket] and populate content
 // need to check the slug against known communities, if no community then redirect to index
 
-import { FC } from "react";
+import MiniCreatePost from "@/components/crumbbasket/MiniCreatePost";
+import { buttonVariants } from "@/components/ui/Button";
+import { INFINIT_SCROLLING_PAGINATION_RESULTS } from "@/config";
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import Link from "next/link";
+import { notFound } from "next/navigation";
 
-interface pageProps {}
+interface PageProps {
+	params: {
+		crumbbasket: string;
+	};
+}
 
-const page: FC<pageProps> = ({}) => {
-	return <div>page</div>;
+const page = async ({ params }: PageProps) => {
+	const { crumbbasket: pageSlug } = params;
+
+	const session = await getAuthSession();
+
+	const existingCrumbbasket = await db.crumbbasket.findFirst({
+		where: {
+			name: pageSlug,
+		},
+		include: {
+			posts: {
+				include: {
+					author: true,
+					votes: true,
+					comments: true,
+					crumbbasket: true,
+				},
+				take: INFINIT_SCROLLING_PAGINATION_RESULTS,
+			},
+		},
+	});
+
+	if (!existingCrumbbasket) return notFound();
+
+	return (
+		<section className='container max-w-5xl mx-auto pt-20'>
+			<div className='flex flex-col justify-center items-center gap-2'>
+				<h1 className='font-bold text-2xl md:text-3xl h-14'>
+					welcome to c/{existingCrumbbasket.name}
+				</h1>
+				<MiniCreatePost session={session} />
+			</div>
+		</section>
+	);
 };
 
 export default page;
+
+// keeping the below for learning, replaced it with
+// if (!existingCrumbbasket) return notFound();
+// -- --
+// using !!! before constName turns constName into a boolean AND looks for the opposite of that converted value
+// !constName => if value exists->boolean=TRUE, else->boolean=FALSE
+// !!constName => if boolean=TRUE -> execute
+// !!!constName => if boolean=FALSE -> execute
+// ^^ - I was wrong on the above existingCrumbbasket returns a value that can be !-checked, no need to convert
+// 	  - was crossing streams thinking i needed to type convert
+// if (!!!existingCrumbbasket) {
+// 	console.log(`INCORRECT COMMUNITY ${pageSlug}`);
+// 	return (
+// 		<section className='container max-w-2xl mx-auto pt-20'>
+// 			<div>
+// 				<h2>INCORRECT CRUMBBASKET</h2>
+// 				<Link
+// 					href={"/"}
+// 					className={`mt-2 ${buttonVariants({ variant: "default" })}`}
+// 				>
+// 					return home
+// 				</Link>
+// 			</div>
+// 		</section>
+// 	);
+// }
---
+++ src/components/crumbbasket/MiniCreatePost.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Session } from "next-auth";
+import { usePathname, useRouter } from "next/navigation";
+import { FC } from "react";
+import UserAvatar from "../UserAvatar";
+import { Input } from "../ui/Input";
+import { Button } from "../ui/Button";
+import { ImageIcon, Link2 } from "lucide-react";
+
+interface MiniCreatePostProps {
+	session: Session | null;
+}
+
+const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
+	const router = useRouter();
+	const pathName = usePathname();
+
+	return (
+		<>
+			<li className='overflow-hidden rounded-md bg-dm-secondary shadow'>
+				<div className='h-full px-6 py-4 flex justify-between gap-6'>
+					<div className='relative'>
+						<UserAvatar
+							user={{
+								name: session?.user.name || null,
+								image: session?.user.image || null,
+							}}
+						/>
+						<span className='absolute bottom-0 right-0 rounded-full w-3 h-3 bg-dm-accent text-text-lightMode ' />
+					</div>
+					<Input
+						readOnly
+						onClick={() => router.push(pathName + "/submit")}
+						placeholder='drop a crumb!'
+					/>
+					<Button
+						variant='custom1'
+						onClick={() => router.push(pathName + "/submit")}
+						className=''
+					>
+						<ImageIcon className='text-text-darkMode hover:text-dm-accent transition-all duration-75' />
+					</Button>
+					<Button
+						variant='custom1'
+						onClick={() => router.push(pathName + "/submit")}
+					>
+						<Link2 className='text-text-darkMode hover:text-dm-accent transition-all duration-75' />
+					</Button>
+				</div>
+			</li>
+		</>
+	);
+};
+
+export default MiniCreatePost;
