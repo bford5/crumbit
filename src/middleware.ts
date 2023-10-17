1d47f719092c795d16b5f4db35e939dfbb9836a9
bforddev7 <bforddev7@gmail.com>
2023-10-17 15:48:07 -0500

    feat: added middleware for nonauth state

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+	const token = await getToken({ req });
+
+	if (!token) return NextResponse.redirect(new URL("/login", req.nextUrl));
+}
+export const config = {
+	matcher: [`/c/:path*/submit`, `/c/create`],
+};
