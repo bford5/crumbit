4238403deb886f3aa6ff7433627f189d3bf88e35
bforddev7 <bforddev7@gmail.com>
2023-10-17 15:35:17 -0500

    chore: removed comment

--- src/app/(crumbbasket)/c/[pageSlug]/layout.tsx
+++ src/app/(crumbbasket)/c/[pageSlug]/layout.tsx
@@ -62,8 +62,6 @@ const Layout = async ({
 	return (
 		<section className='sm:container max-w-7xl mx-auto h-full pt-12'>
 			<div>
-				{/* TODO: btn to navigate back */}
-
 				<div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6'>
 					<div className='flex flex-col col-span-2 space-y-6'>{children}</div>
 					{/* INFOSIDEBAR */}
